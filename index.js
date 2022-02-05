46b4739252cdc4b3d13b8371d275ba6119116449
gyataro <teoh6g@gmail.com>
2022-02-05 22:15:14 +0800

    Added blog CRUD application

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { 
+    MONGO_USER, 
+    MONGO_PASSWORD, 
+    MONGO_IP, 
+    MONGO_PORT 
+} = require("./config/config");
+
+const postRouter = require("./routes/postRoutes");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
 mongoose
-    .connect("mongodb://root:root@mongo:27017/?authSource=admin")
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to DB!"))
     .catch((e) => console.log(e));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("<h2>Hello World!</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 const port = process.env.PORT || 3000;
 
